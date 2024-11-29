d86a26cbc8d4548d340b4e3f34efc35143ec1505
lilcryptotech <lilcryptotech@gmail.com>
2024-11-29 13:40:23 -0500

    renamed a file to  beacon and added a main file checkpoint

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import inquirer
+import os
+
+python_files = [file for file in os.listdir('.') if file.endswith('.py')]
+
+if not python_files:
+    print("No Python files found in the current directory.")
+    exit()
+
+choices = [f"{index + 1}. {file}" for index, file in enumerate(python_files)]
+
 questions = [
-  inquirer.List('size',
-                message="which file do you what to open?",
-                choices=[''],
-            ),
+    inquirer.List(
+        'choice',
+        message="Which file do you want to open and run?",
+        choices=choices,  
+    ),
 ]
+
+
 answers = inquirer.prompt(questions)
+
+selected_index = int(answers['choice'].split('.')[0]) - 1
+selected_file = python_files[selected_index]
+
+
+print(f"You selected: {selected_file}")
+
+try:
+    exec(open(selected_file).read())
+except Exception as e:
+    print(f"An error occurred: {e}")
+    
